5f8443ba4992e7379774e3d081cf20d66c563d7d
ame <xr2117@126.com>
2019-10-17 21:20:47 +0800

    加入集群监控

--- xri-hystrix-dashboard/src/main/java/com/frame/hystrix/dashboard/HystrixDashboardApp.java
+++ xri-hystrix-dashboard/src/main/java/com/frame/hystrix/dashboard/HystrixDashboardApp.java
@@ -1,27 +1,33 @@
 package com.frame.hystrix.dashboard;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author : Crazy.X
  * @Date : 2019/10/16
- * <p>
+ *
  * HystrixDashboard 监控ui
  * @Param localhost Hystrix Dashboard Service Address
  * @Param port      Hystrix Dashboard Service Address
  * {@link} http://localhost:port/hystrix
- * <p>
- * 监控ui 表单
+ *
+ * 监控集群ui 表单
  * @Param localhost Micro Service Address
  * @Param port      Micro Service Port
- * {@link} http://localhost:port/actuator/hystrix.stream
+ * {@link} http://localhost:port/turbine.stream
  */
+@EnableEurekaClient
+@RestController
+@EnableHystrix
 @EnableHystrixDashboard
-@SpringBootApplication
-@EnableDiscoveryClient
+@EnableTurbine
+@SpringCloudApplication
 public class HystrixDashboardApp {
     public static void main(String[] args) {
         SpringApplication.run(HystrixDashboardApp.class, args);
